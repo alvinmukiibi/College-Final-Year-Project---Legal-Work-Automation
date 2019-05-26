6f478ecc57e1105efef68b8b426962e9091f4517
alvinmukiibi <mukikelly792@gmail.com>
2019-05-26 12:35:16 +0300

    Favicon for out and new logo

--- resources/views/layouts/partials/headout.blade.php
+++ resources/views/layouts/partials/headout.blade.php
@@ -4,7 +4,8 @@
 	<title>L-WAT System</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
-   
+    <link rel="shortcut icon" href="{{asset('dist/img/favicon.ico')}}" type="image/x-icon">
+
     <link rel="stylesheet" href="{{asset('css/animate.css')}}">
 	<link rel="stylesheet" href="{{asset('css/icomoon.css')}}">
 	<link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
@@ -17,10 +18,10 @@
 	<link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}">
 	<link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
 	<link rel="stylesheet" href="{{asset('css/magnific-popup.css')}}">
-	<link rel="stylesheet" href="{{asset('css/jquery-ui.css')}}">				
-	<link rel="stylesheet" href="{{asset('css/nice-select.css')}}">							
+	<link rel="stylesheet" href="{{asset('css/jquery-ui.css')}}">
+	<link rel="stylesheet" href="{{asset('css/nice-select.css')}}">
 	<link rel="stylesheet" href="{{asset('css/animate.min.css')}}">
-	<link rel="stylesheet" href="{{asset('css/owl.carousel.css')}}">				
+	<link rel="stylesheet" href="{{asset('css/owl.carousel.css')}}">
 	<link rel="stylesheet" href="{{asset('css/main.css')}}">
 	<link rel="stylesheet" href="{{asset('css/custom.css')}}">
 	</head>
